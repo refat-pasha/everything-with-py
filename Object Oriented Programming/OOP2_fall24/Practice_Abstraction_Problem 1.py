07b70c957c89ba5fa4afd8674f4c47b31d04eb94
turtle <refatpasha567@gmail.com>
2024-11-25 01:30:47 +0600

    BankAccount and SavlingAccount with deposit, withdraw and get_balance(display) mehtods

---
+++ Object Oriented Programming/OOP2_fall24/Practice_Abstraction_Problem 1.py
@@ -0,0 +1,42 @@
+# Problem 1:
+
+from abc import ABC,abstractmethod
+class BankAccount(ABC):
+    @abstractmethod
+    def deposit(self):
+        pass
+    @abstractmethod
+    def withdraw(self):
+        pass
+    @abstractmethod
+    def get_balance(self):
+        pass
+class SavingAccount(BankAccount):
+    def __init__(self, dep_bal, with_bal, bal=0):
+        self.dep_bal = dep_bal
+        self.with_bal = with_bal
+        self.bal = bal
+        self.deposit()
+        self.withdraw()
+
+    def deposit(self):
+        if self.dep_bal > 0:
+            self.bal += self.dep_bal
+
+    def withdraw(self):
+        if self.with_bal > self.bal:
+            print("Insufficient Balance")
+        else:
+            self.bal -= self.with_bal
+
+    def get_balance(self):
+        print(f"Balance after Deposit: {self.dep_bal}\n"
+              f"Balance after Withdraw: {self.with_bal}\n"
+              f"Current Balance: {self.bal}")
+
+
+
+obj = SavingAccount(2000,200)
+obj.get_balance()
+
+
