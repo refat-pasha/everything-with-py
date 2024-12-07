6ed96565f16f31761e4ccbf6c672428d1c71f3a7
turtle <refatpasha567@gmail.com>
2024-12-07 23:17:54 +0600

    Create great_less.py

---
+++ Object Oriented Programming/OOP2_fall24/great_less.py
@@ -0,0 +1,11 @@
+a = int(input("enter a number: "))
+b = int(input("enter 2nd number: "))
+
+def great_less(a,b):
+    if a> b:
+        print("a in greater than b!")
+    else:
+        print("b is greater than a!")
+
+
+great_less(a, b)
