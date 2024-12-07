b220b12bbea29a3a3656b1175ab094cb2644dc2c
turtle <refatpasha567@gmail.com>
2024-12-07 23:22:20 +0600

    Update salary.py

--- Object Oriented Programming/OOP2_fall24/salary.py
+++ Object Oriented Programming/OOP2_fall24/salary.py
@@ -1,1 +1,15 @@
 x = int(input("enter salary: "))
+
+def bonus(x):
+    if x>= 20000 <= 30000:
+        bonus = x * .05
+        return bonus + x
+    elif x> 30000:
+        bonus = x * .1
+        return bonus + x
+    elif x< 20000:
+        return " no Bonus"
+    else:
+        return "not valid salary!"
+
+print(bonus(x))
